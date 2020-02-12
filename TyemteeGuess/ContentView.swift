ea762d7dcf009a7c1bb51bafa3644ad331c324c9
Viettasc <viettasc@gmail.com>
2020-02-12 09:12:45 +0700

    Initial Commit

---
+++ TyemteeGuess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TyemteeGuess
+//
+//  Created by Viettasc on 2/12/20.
+//  Copyright © 2020 Viettasc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
